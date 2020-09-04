e5b6216a7fb7b2874a050be5133581c9c3f99c7e
tnkillen <57112401+tnkillen@users.noreply.github.com>
2020-09-04 17:28:34 -0500

    Individual Notifications
    Added more exception handling and set notifications to show item name and send to the relevent user only.  Checks for invalid urls will send user a push notification saying which listing has an invalid url. If a http request fails, it will recheck the item up to 3 times. If it still cannot successfully scrape the price, the url will be logged to follow up.

--- ConceptScraper/ConceptScraper.py
+++ ConceptScraper/ConceptScraper.py
@@ -1,35 +1,108 @@
 import requests
+from requests.exceptions import MissingSchema, InvalidURL, ChunkedEncodingError, ConnectionError, ConnectTimeout
 from bs4 import BeautifulSoup
 import time
+import six
+import re
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import firestore
+from firebase_admin import db
 
+# Firebase Setup
+cred = credentials.Certificate('./ServiceKeys.json')
+default_app = firebase_admin.initialize_app(cred, {'databaseURL': 'https://tracking-51514.firebaseio.com/'})
+root = db.reference()
 
-url = 'https://www.amazon.com/S8IN4O/dp/B07L5GDTYY/ref=sr_1_3?crid=1N6LKU38UPP19&dchild=1&keywords=kindle+oasis&qid=1595706601&sprefix=kindle+o%2Caps%2C172&sr=8-3'
+# Webpage Setup
 headers = {
     "User-Agent": 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.89 Safari/537.36'}
+# Class setup
+class Listing:
+    def __init__(self, userid, url, desiredPrice, itemName):
+        self.userid = userid
+        self.url = url
+        self.desiredPrice = desiredPrice
+        self.itemName = itemName
+        self.tryCount = 0
 
+def check_price(userid, url, desiredPrice, itemName):
+    try:
+        try:
+            page = requests.get(url, headers=headers)
+        except (MissingSchema, InvalidURL):
+            send_mail(userid,"Your Watch Listing for " + itemName + " Has an Invalid URL")
+            return 0
+        soup = BeautifulSoup(page.content, 'html.parser')
+        #try:
+        #     title = soup.find(id="productTitle").get_text()
+        #except TypeError:
+        #    try:
+        #        title = soup.find(id="productTitle").get_text()
+        try:
+            price = soup.find(id="priceblock_ourprice").get_text()
+        except AttributeError:
+            try:
+                price = soup.find(id="newBuyBoxPrice").get_text()
+            except AttributeError:
+                try:
+                    price = soup.find(id="price_inside_buybox").get_text()
+                except AttributeError:
+                    try:
+                        price = soup.find(id="price").get_text()
+                    except AttributeError:
+                        return Listing(userid, url, desiredPrice, itemName)
+    except (ChunkedEncodingError, ConnectionError, ConnectTimeout) as e:
+        print("ERROR: " + str(e))
+        return 1
 
-def check_price():
-    page = requests.get(url, headers=headers)
+    if isinstance(price, six.string_types):
+        price = float(re.sub(r'[^0-9\.]','', price))
 
-    soup = BeautifulSoup(page.content, 'html.parser')
+    converted_price = float(price)
+    
+    if(converted_price <= desiredPrice):
+        send_mail(userid, itemName + " Has Dropped in Price on Amazon!")
 
-    title = soup.find(id="productTitle").get_text()
-    price = soup.find(id="priceblock_ourprice").get_text()
-    converted_price = float(price[1:])
-    if(converted_price < 360):
-        send_mail()
-
-    print(title.strip())
+    #print(title.strip())
     print(converted_price)
+    return 0
 
 
-def send_mail():
+def send_mail(userid, message):
     data = {"app_id": "02fed716-2742-4d0e-b796-4d9b86c3dafb",
-        "included_segments": ["All"], "contents": {"en": "An Item You're Watching Has Reached Your Desired Price!"}}
+        "include_player_ids": [userid], "contents": {"en":message}}
     requests.post("https://onesignal.com/api/v1/notifications",    headers={
               "Authorization": "Basic MzFkMzQ3ZDAtZGUyZC00OTUxLWI0ZTUtYzJmNjE2NGMxNWFk"},    json=data)
 
+def run_listings():
+   failedListings = []
+   firebaseFetch = root.get()
+   for userid,itemInfo in firebaseFetch.items():
+      for key in itemInfo:
+          hasReturned = check_price(userid, itemInfo[key]['itemURL'], itemInfo[key]['desiredPrice'], itemInfo[key]['itemName'])
+          if type(hasReturned) is Listing:
+              failedListings.append(hasReturned)
+              
+
+   while(len(failedListings) > 0):
+       for x in failedListings:
+           hasReturned = check_price(x.userid, x.url, x.desiredPrice, x.itemName)
+           if hasReturned == 0:
+               failedListings.remove(x)
+           elif x.tryCount >= 3:
+                print("Check Price Scrape Exceptions on : " + x.url)
+                failedListings.remove(x)
+           else:
+               x.tryCount += 1
+
+
 
 while(True):
-    check_price()
+    print("\nChecking Prices for all Users: \n")
+    run_listings()
     time.sleep(60 * 60)  # runs check every hour
+
+
+        
+    
